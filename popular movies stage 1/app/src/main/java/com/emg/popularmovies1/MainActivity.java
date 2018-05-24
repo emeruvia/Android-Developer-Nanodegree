753999cb9ab06f06d6590d953c8e12da102558a7
Edgar Meruvia <emeruvia@gmail.com>
2018-05-24 19:36:19 -0400

    started project

--- popular movies stage 1/app/src/main/java/com/emg/popularmovies1/MainActivity.java
+++ popular movies stage 1/app/src/main/java/com/emg/popularmovies1/MainActivity.java
@@ -2,12 +2,17 @@ package com.emg.popularmovies1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+  }
 }
