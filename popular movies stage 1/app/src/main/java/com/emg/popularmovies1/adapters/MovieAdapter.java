b9dfabd88ef61bde5945af77ff819682bd408798
Edgar Meruvia <emeruvia@gmail.com>
2018-06-11 22:38:33 -0400

    Refactor fast-adapter code. Adding regular recycler views

--- popular movies stage 1/app/src/main/java/com/emg/popularmovies1/adapters/MovieAdapter.java
+++ popular movies stage 1/app/src/main/java/com/emg/popularmovies1/adapters/MovieAdapter.java
@@ -1,63 +1,11 @@
 package com.emg.popularmovies1.adapters;
 
-import android.support.annotation.NonNull;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.emg.popularmovies1.R;
 import com.emg.popularmovies1.models.Movie;
-import com.mikepenz.fastadapter.FastAdapter;
-import com.mikepenz.fastadapter.items.AbstractItem;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class MovieAdapter extends AbstractItem<MovieAdapter, MovieAdapter
-    .ViewHolder> {
+public class MovieAdapter {
 
   private List<Movie> movieList;
-
-  @NonNull
-  @Override
-  public ViewHolder getViewHolder(View v) {
-    return null;
-  }
-
-  @Override
-  public int getType() {
-    return 0;
-  }
-
-  @Override
-  public int getLayoutRes() {
-    return R.layout.activity_main_moviecardview;
-  }
-
-  protected static class ViewHolder extends FastAdapter
-      .ViewHolder<MovieAdapter> {
-
-    @BindView(R.id.posterImageView)
-    ImageView poster;
-    @BindView(R.id.titleTextView)
-    TextView movieTitle;
-
-    public ViewHolder(View view) {
-      super(view);
-      ButterKnife.bind(this, view);
-    }
-
-    @Override
-    public void bindView(MovieAdapter item, List<Object> payloads) {
-
-    }
-
-    @Override
-    public void unbindView(MovieAdapter item) {
-
-    }
-  }
 
 }
