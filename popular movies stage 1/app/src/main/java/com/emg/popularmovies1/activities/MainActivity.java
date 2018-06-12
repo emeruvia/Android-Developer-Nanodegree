3192c62069a7308f599075278de19e56af5c19f8
Edgar Meruvia <emeruvia@gmail.com>
2018-06-12 00:30:13 -0400

    OnClickListener for the listview implemented

--- popular movies stage 1/app/src/main/java/com/emg/popularmovies1/activities/MainActivity.java
+++ popular movies stage 1/app/src/main/java/com/emg/popularmovies1/activities/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.emg.popularmovies1.R;
 import com.emg.popularmovies1.adapters.MovieAdapter;
+import com.emg.popularmovies1.interfaces.ClickListener;
 import com.emg.popularmovies1.interfaces.Constants;
 import com.emg.popularmovies1.interfaces.MovieClient;
 import com.emg.popularmovies1.models.Movie;
@@ -24,11 +26,11 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ClickListener {
 
   @BindView(R.id.movies_rv)
   RecyclerView recyclerView;
-
+  private List<Movie> movieList;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -39,22 +41,21 @@ public class MainActivity extends AppCompatActivity {
 
     Retrofit retrofit = RetrofitClient.getClient(Constants.BASE_URL);
     MovieClient client = retrofit.create(MovieClient.class);
-    Call<Movies> call = client.movies(Constants.SORT_POPULAR, getResources().getString(R.string.API_KEY), 1);
+    Call<Movies> call = client.movies(Constants.SORT_TOP_RATED
+        , getResources().getString(R.string.API_KEY), 1);
     call.enqueue(new Callback<Movies>() {
       @Override
       public void onResponse(Call<Movies> call, Response<Movies> response) {
         Movies movies = response.body();
         Log.d("Page", String.valueOf(movies.getPage()));
-        List<Movie> movieList = movies.getMovieList();
+        movieList = movies.getMovieList();
         for (Movie m : movieList) {
           Log.d("Movie",
               m.getTitle() + ", "
-              + m.getImagePath() + "\n"
+                  + m.getImagePath() + "\n"
           );
         }
-        MovieAdapter viewAdapter = new MovieAdapter(movieList);
-        recyclerView.setAdapter(viewAdapter);
-
+        success(movieList);
       }
 
       @Override
@@ -64,4 +65,14 @@ public class MainActivity extends AppCompatActivity {
     });
   }
 
+  private void success(List<Movie> movieList) {
+    MovieAdapter viewAdapter = new MovieAdapter(movieList);
+    viewAdapter.setClickListener(this);
+    recyclerView.setAdapter(viewAdapter);
+  }
+
+  @Override
+  public void itemClicked(View view, int position) {
+    Log.d("Movie Clicked", movieList.get(position).getTitle());
+  }
 }
