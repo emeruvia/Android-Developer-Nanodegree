56f70153b62a9a25c6f3a45dda68dd6e9efcefcc
Edgar Meruvia Garron <emeruvia@gmail.com>
2018-07-31 12:55:34 -0400

    Added fragments for top rated and popular movies, updated the way the fragments are being drawn into the screen

--- PopularMovies1/app/src/main/java/com/emg/popularmovies1/MainActivity.java
+++ PopularMovies1/app/src/main/java/com/emg/popularmovies1/MainActivity.java
@@ -2,14 +2,19 @@ package com.emg.popularmovies1;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.emg.popularmovies1.fragments.PopularMoviesFragment;
+import com.emg.popularmovies1.fragments.TopRatedFragment;
 import com.emg.popularmovies1.utils.Constants;
 import com.emg.popularmovies1.utils.Network;
 
@@ -21,6 +26,9 @@ public class MainActivity extends AppCompatActivity
 
   @BindView(R.id.toolbar) Toolbar mToolbar;
   @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
+  @BindView(R.id.nav_view) NavigationView mNavigationView;
+  private FragmentManager fragmentManager;
+  private Fragment fragment;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,14 @@ public class MainActivity extends AppCompatActivity
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
     setSupportActionBar(mToolbar);
+    fragmentManager = getSupportFragmentManager();
+    fragment = new PopularMoviesFragment();
+    fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+//    new Network(this, Constants.SORT_POPULAR);
     ActionBar actionBar = getSupportActionBar();
     actionBar.setDisplayHomeAsUpEnabled(true);
     actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-    new Network(this, Constants.SORT_POPULAR);
+    mNavigationView.setNavigationItemSelectedListener(this);
   }
 
   @Override
@@ -48,8 +60,23 @@ public class MainActivity extends AppCompatActivity
   public boolean onNavigationItemSelected(@NonNull MenuItem item) {
     item.setChecked(true);
     mDrawerLayout.closeDrawers();
-
-
+    Log.d("MenuItem", item.getTitle() + " clicked");
+    switch(item.getItemId()) {
+      case R.id.sort_by_popular:
+        fragment = new PopularMoviesFragment();
+//        new Network(this, Constants.SORT_POPULAR);
+        break;
+      case R.id.sort_by_top_rated:
+        fragment = new TopRatedFragment();
+//        new Network(this, Constants.SORT_TOP_RATED);
+        break;
+      default:
+        fragment = new PopularMoviesFragment();
+//        new Network(this, Constants.SORT_POPULAR);
+        break;
+    }
+    fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+    setTitle(item.getTitle());
     return true;
   }
 }
